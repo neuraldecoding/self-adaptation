3b6dc73bd47d632ec05f2e667c0ce7c019d4a1f5
Rolly Maulana Awangga <awangga@gmail.com>
2026-08-01 09:12:00 +0700

    add kma

---
+++ kma/Main2D.m
@@ -0,0 +1,87 @@
+%=================================================================================
+% Komodo Mlipir Algorithm (KMA) version 1.0.0
+%
+% Developed in MATLAB R2017a based on the following paper:
+%
+% S. Suyanto, A.A. Ariyanto, A.F. Ariyanto, Komodo Mlipir Algorithm 
+% Applied Soft Computing, 2021, 108043, https://doi.org/10.1016/j.asoc.2021.108043
+%
+% Main program with 2D Visualization
+% 
+% Parameters:
+% 1. FunctionID    : Identity of the benchmark function
+% 2. Dimension     : dimension of the function to optimize
+% 3. MaxNumEva     : Maximum Number of Evaluations
+% 4. PopSize       : Population Size (number of Komodo individuals)
+% 5. MinAdaPopSize : Minimum Adaptive PopSize
+% 6. MaxAdaPopSize : Maximum Adaptive PopSize
+%
+% Updated 23 November 2021 by Prof. Dr. Suyanto, S.T., M.Sc.
+% School of Computing, Telkom University
+% Jl. Telekomunikasi No 1 Terusan Buah Batu, Bandung 40257, Indonesia
+% Email: suyanto@telkomuniversity.ac.id 
+% Website: https://suyanto.staff.telkomuniversity.ac.id
+%=================================================================================
+
+clc
+clear
+close all
+
+global FunctionID Dimension Nvar Ra Rb FthresholdFX
+global PopSize MinAdaPopSize MaxAdaPopSize MaxNumEva
+
+%% Set of parameters
+FunctionID    = 1;             % Identity of the bencmark function
+Dimension     = 50;            % Dimension can scaled up to thousands for the functions F1-F13, but it is fixed for F14-F23
+MaxNumEva     = 25000;         % Maximum number of evaluations
+PopSize       = 5;             % Population Size (number of Komodo individuals)
+MinAdaPopSize = PopSize * 4;   % Minimum Adaptive PopSize
+MaxAdaPopSize = PopSize * 40;  % Maximum Adaptive PopSize
+
+%% Get a benchmark function and call KMA to get its global optimum solution
+[Nvar,Ra,Rb,FthresholdFX] = GetFunction;
+Ra = ones(1,Nvar) .* Ra;
+Rb = ones(1,Nvar) .* Rb;
+
+[BestIndividual,OptVal,NumEva,fopt,fmean,ProcTime,EvoPopSize] = KMA2D;
+
+%% Report the result of KMA
+disp(['Function              = F' num2str(FunctionID)])
+disp(['Dimension             = ' num2str(Dimension)])
+disp(['Number of evaluations = ' num2str(NumEva)])
+disp(['Processing time (sec) = ' num2str(ProcTime)])
+disp(['Global optimum        = ' num2str(FthresholdFX,10)])
+disp(['Actual solution       = ' num2str(OptVal,10)])
+disp(['Best individual       = ' num2str(BestIndividual)])
+
+%% Visualize the convergence curve of KMA
+set(figure, 'position', [100,100,1000,600]);
+hold on
+plot(fopt,'r-')
+plot(fmean,'b--')
+title(['Convergence curve of Function F',num2str(FunctionID)]);
+xlabel('Generation');
+ylabel('Fitness');
+legend('Best fitness','Mean fitness')
+hold off
+
+%% Visualize the log convergence curve of KMA
+if fopt(end) >= 0
+    set(figure, 'position', [100,100,1000,600]);
+    hold on
+    plot(log10(fopt),'r-')
+    plot(log10(fmean),'b--')
+    title(['Log convergence curve of Function F',num2str(FunctionID)]);
+    xlabel('Generation');
+    ylabel('Log Fitness');
+    legend('Best fitness','Mean fitness')
+    hold off
+end
+
+%% Visualize the fixed and self-adaptation of population size of KMA
+set(figure, 'position', [100,100,1000,600]);
+plot(EvoPopSize)
+axis([1 length(EvoPopSize) 0 MaxAdaPopSize+5]);
+title(['Fixed and self-adaptive population size in the first and second stage for Function F',num2str(FunctionID)]);
+xlabel('Generation');
+ylabel('Population size (Number of individuals)');
---
+++ kma/Readme.txt
@@ -0,0 +1,18 @@
+Komodo Mlipir Algorithm (KMA) version 1.0.0
+
+Developed in MATLAB R2017a based on the following paper:
+
+S. Suyanto, A.A. Ariyanto, A.F. Ariyanto, Komodo Mlipir Algorithm 
+Applied Soft Computing, 2021, 108043, https://doi.org/10.1016/j.asoc.2021.108043
+
+Updated 23 November 2021 by Prof. Dr. Suyanto, S.T., M.Sc.
+School of Computing, Telkom University
+Jl. Telekomunikasi No 1 Terusan Buah Batu, Bandung 40257, Indonesia
+Email: suyanto@telkomuniversity.ac.id 
+Website: https://suyanto.staff.telkomuniversity.ac.id
+
+How to use the source code:
+
+1. Open in Matlab R2017a or other compatible versions.
+2. Run Main2D.m to get the optimum solution to a benchmark function (defined by FunctionID and Dimension) with two-dimensional visualization of KMA evolution.
+3. Run Main3D.m to get the optimum solution to a benchmark function (defined by FunctionID and Dimension) with three-dimensional visualization of KMA evolution.
